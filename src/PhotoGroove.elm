76ecb1db8de746fd5919b55a3373d95478aa01b0
ciomarabanu <mara.mocanita@gmail.com>
2020-10-20 21:30:43 +0300

    chapter 1 completed

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,19 +1,59 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Browser
+import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
 
 
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+        , div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedThumbnail) model.thumbnails)
+        , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedThumbnail) ] []
         ]
 
 
+viewThumbnail selectedThumbnail thumb =
+    img
+        [ src (urlPrefix ++ thumb.fileName)
+        , classList [ ( "selected", selectedThumbnail == thumb.fileName ) ]
+        , onClick { action = constants.clickedThumbnailAction, thumbnail = thumb.fileName }
+        ]
+        []
+
+
+initialModel =
+    { thumbnails =
+        [ { fileName = "1.jpeg" }
+        , { fileName = "2.jpeg" }
+        , { fileName = "3.jpeg" }
+        ]
+    , selectedThumbnail =
+        "1.jpeg"
+    }
+
+
+update msg model =
+    if msg.action == constants.clickedThumbnailAction then
+        { model | selectedThumbnail = msg.thumbnail }
+
+    else
+        model
+
+
+constants =
+    { clickedThumbnailAction = "wtf"
+    }
+
+
 main =
-    view "no model yet"
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
